19053d10241f02963158d54687d9a84406869e1a
Luckqing5148 <Luckqing5148>
2017-01-16 17:12:44 +0800

    1111111111

--- src/scripts/xqg.js
+++ src/scripts/xqg.js
@@ -1,10 +1,29 @@
+import Footer from './footer'
 class Xqg extends React.Component{
   render(){
     return(
           <div id='xqg'>
-              
+              <HeaderXqg />
+              <div id="contentXqg">
+                  xqgssssssssssssss
+              </div>
+              <Footer />
           </div>
-    )
+    ) /
   }
+}
+
+class HeaderXqg extends React.Component{
+	render(){
+		return(
+			<div id="header">
+				<ul>
+					<li></li>
+					<li>乡情馆</li>
+					<li></li>
+				</ul>
+			</div>
+		)
+	}
 }
 export default Xqg
